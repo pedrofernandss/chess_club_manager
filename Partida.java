fa22c0f8dfba6f1b1f743e40d32df1ee89600f71
Pedro <pedrofernandes.olv@gmail.com>
2024-07-29 17:37:31 -0300

    relacionamento de Equipe com Federacao, Jogadores e Treinador

---
+++ Partida.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Partida {
+    private String categoria;
+    private Date dataPartida;
+    private Jogador jogador1;
+    private Jogador jogador2;
+    private String local;
+    private String resultado;
+
+
+}
